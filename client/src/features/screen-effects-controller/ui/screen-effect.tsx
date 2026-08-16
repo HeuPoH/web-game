5c51c6cfa1e52b103ecefe2a8c8391863705bd54
Vadim Knyazev <mdiv551@gmail.com>
2026-08-16 21:31:41 +0300

    update screen effect

--- client/src/features/screen-effects-controller/ui/screen-effect.tsx
+++ client/src/features/screen-effects-controller/ui/screen-effect.tsx
@@ -1,6 +1,5 @@
-import { cn } from '../../../shared/utils';
 import classes from './style.module.css';
 
 export const ScreenEffect: React.FC<{ ref: React.Ref<HTMLDivElement> }> = ({ ref }) => {
-  return <div ref={ref} className={cn(classes.effectContainer, classes.jumpEffect)} />;
+  return <div ref={ref} className={classes.effectContainer} />;
 };
